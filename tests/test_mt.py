bc2c1565ad7edb62bdea240cbab12483843d938c
Richard Holmes <richard@shedc.uk>
2021-03-11 13:34:29 +0000

    Testing Again

--- tests/test_mt.py
+++ tests/test_mt.py
@@ -1,5 +1,5 @@
 """Various Tests Scripts for testing the Fixtures in the mt folder"""
 
 async def test_loadall():
-    """Test a load of all the data."""
+    """Test a load of all the data"""
     pass

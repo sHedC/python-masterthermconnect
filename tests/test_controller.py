52a674cb9552a9e80c5414e55a8b681c7a68ba01
Richard Holmes <richard@shedc.uk>
2021-03-11 13:06:17 +0000

    First Commit, added pads/ main data

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -47,6 +47,8 @@ async def test_connect_unsupported():
         with pytest.raises(MasterThermUnsupportedRole):
             assert await controller.connect() == True
 
+    assert len(mock_apiconnect.mock_calls) == 1
+
 async def test_connect_failure():
     """Test the Controller Invalid Login Connection."""
     controller = Controller(ClientSession(), VALID_LOGIN["uname"], VALID_LOGIN["upwd"])
@@ -56,7 +58,7 @@ async def test_connect_failure():
         side_effect=MasterThermAuthenticationError("1", "Invalid user name or password")
     ) as mock_apiconnect:
         with pytest.raises(MasterThermAuthenticationError):
-            result = await controller.connect()
+            await controller.connect()
 
     assert len(mock_apiconnect.mock_calls) > 0
 
@@ -69,7 +71,7 @@ async def test_connect_error():
         side_effect=MasterThermConnectionError("500", "Some Other Error")
     ) as mock_apiconnect:
         with pytest.raises(MasterThermConnectionError):
-            result = await controller.connect()
+            await controller.connect()
     
     assert len(mock_apiconnect.mock_calls) > 0
 
@@ -82,9 +84,6 @@ async def test_get_info_data():
         "masterthermconnect.connection.Connection.connect",
         return_value=mockconnect.connect(),
     ) as mock_apiconnect, patch(
-        "masterthermconnect.connection.Connection.isConnected",
-        return_value=True
-    ) as mock_isconnected, patch(
         "masterthermconnect.connection.Connection.getDeviceInfo",
         side_effect=mockconnect.getDeviceInfo
     ) as mock_getdeviceinfo, patch(
@@ -94,17 +93,23 @@ async def test_get_info_data():
         assert await controller.connect() == True
 
     assert len(mock_apiconnect.mock_calls) > 0
-    assert len(mock_isconnected.mock_calls) > 0
     assert len(mock_getdeviceinfo.mock_calls) > 0
     assert len(mock_getdevicedata.mock_calls) > 0
 
     assert controller.getDevices() != {}
 
     info = controller.getDeviceInfo("1234", "1")
+    dataRaw = controller.getDeviceRegisters("1234","1")
     data = controller.getDeviceData("1234","1")
 
     assert info["country"] == "UK"
-    assert data["A_500"] == "46.2"
+    assert dataRaw["A_500"] == "46.2"
+    assert data["on"] == True
+    assert data["outside_temp"] == 4.2
+    assert data["requested_temp"] == 46.2
+    assert data["pads"]["pada"]["name"] == "HW-AN-"
+    assert data["pads"]["pada"]["on"] == True
+    assert "padc" not in data["pads"]
 
 async def test_getdata_update():
     """Test getting the data and getting an update."""
@@ -126,22 +131,27 @@ async def test_getdata_update():
         side_effect=mockconnect.getDeviceData
     ) as mock_getdevicedata:
         assert await controller.connect() == True
+
+        data = controller.getDeviceData("1234","1")
+        assert data["requested_temp"] == 46.2
         assert await controller.refresh() == True
 
     assert len(mock_apiconnect.mock_calls) == 1
-    assert len(mock_isconnected.mock_calls) == 2
+    assert len(mock_isconnected.mock_calls) == 1
     assert len(mock_getdeviceinfo.mock_calls) == 2
     assert len(mock_getdevicedata.mock_calls) == 2
 
     assert controller.getDevices() != {}
 
+    dataRaw = controller.getDeviceRegisters("1234","1")
+    dataUpdate = controller.getDeviceRegisters("1234", "1", lastUpdated=True)
     data = controller.getDeviceData("1234","1")
-    dataUpdate = controller.getDeviceData("1234", "1", lastUpdated=True)
 
-    assert data["A_500"] == "40.7"
+    assert dataRaw["A_500"] == "40.7"
     assert dataUpdate["A_500"] == "40.7"
     assert "I_418" not in dataUpdate
-    assert "I_418" in data
+    assert "I_418" in dataRaw
+    assert data["requested_temp"] == 40.7
 
 
 

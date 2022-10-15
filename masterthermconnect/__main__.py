3ba30743435d7845dbf11c46d6ebf2cc2ee3dabb
Richard Holmes <richard@shedc.uk>
2022-10-15 19:14:20 +0000

    Setup for PyPI

---
+++ masterthermconnect/__main__.py
@@ -0,0 +1,8 @@
+"""Main Program, for Testing Mainly"""
+
+def main():
+    """Allow for some testing of connections from Command Line."""
+    print("Python MasterTherm Connect Module.")
+
+if __name__ == "__main__":
+    main()

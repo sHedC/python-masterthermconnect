57f466222097dbc2640b2bc143b1976319ebb531
Richard Holmes <richard@shedc.uk>
2022-10-16 03:30:41 +0000

    Preparing bumpver usage

--- masterthermconnect/__main__.py
+++ masterthermconnect/__main__.py
@@ -1,8 +1,9 @@
 """Main Program, for Testing Mainly"""
+from masterthermconnect.__version__ import __version__
 
 def main():
     """Allow for some testing of connections from Command Line."""
-    print("Python MasterTherm Connect Module.")
+    print("Python MasterTherm Connect Module, Version="+__version__)
 
 if __name__ == "__main__":
     main()
